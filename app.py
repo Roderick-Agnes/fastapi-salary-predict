02e0b0d7cc853d28ab83037141f768e4f3724511
Roderick <m.contactqc@gmail.com>
2022-10-14 14:21:04 +0700

    completed single file handle

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from urllib import response
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File, UploadFile
+import shutil
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
+
 from models.main import *
+
 
 # app config
 app = FastAPI()
@@ -26,19 +29,37 @@ app.add_middleware(
 @app.get("/", tags=["Root"])
 async def read_root():
   return { 
-    "message": "Welcome to my notes application, use the /docs route to proceed"
+    "message": "Welcome to SALARY PREDICT API!",
    }
 
-@app.post('/salary')
-async def predictor(request: Request):
+@app.post('/salary_with_formdata')
+async def predictor_with_formdata(request: Request):
+    # handle with data type is form data
     data = await request.json()
-    if data:
-        type = data.get('type')
+    try:
+        # get data object from request
         data = data.get('data')
-        if type == 'single':
-            response = build_and_predict(data)
-            return response
-        else:
-            print('multi handle')
-            return '1'
-    
+
+        # build model and salary prediction
+        response = build_and_predict(data)
+        return response
+    except Exception:
+        return {'message': "Error to fecth data of salary with form data"}
+
+        
+@app.post("/salary_with_singlefile")
+def upload(file: UploadFile = File()):
+    # handle with data type is file data, include: single file or multiple files
+    try:
+        file_location = f"files/{file.filename}"
+        with open(file_location, "wb+") as file_object:
+            shutil.copyfileobj(file.file, file_object)
+        print(file_location)
+        response = build_and_predict(file_location, 'singlefile')
+        print('response: ', response)
+        return response
+        # return {"message": "Success to upload"}
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
--- models/main.py
+++ models/main.py
@@ -2,14 +2,14 @@ from utils.libs import *
 
 # loading dataset
 def load_dataset():
-    dataset = pd.read_csv("dataset/salary_dataset - Sheet1.csv")
-    return dataset
+    return pd.read_csv("dataset/salary_dataset - Sheet1.csv")
 
 # cleaning dataset
 def clean_dataset():
     dataset = load_dataset()
     # Clean null or NaN values from data frame using dropna().
     # change type of salary column to float
+    
     index = 0
     dataset['salary'].dropna()
     for element in dataset['salary']:
@@ -32,16 +32,11 @@ def clean_dataset():
     dataset['year_experience'] = pd.to_numeric(dataset['year_experience'], errors='coerce')
     dataset['year_experience'].dropna()
 
-    # dataset['knowledge'].dropna()
-    # dataset['technical'].dropna()
-    # dataset['logical'].dropna()
-    # dataset['year_experience'].dropna()
-
     dataset = dataset.dropna()
     return dataset
 
 # Building Model
-def build_and_predict(data):
+def build_and_predict(data, type: str = 'formdata'):
     dataset = clean_dataset()
     
     x = dataset.drop('salary', axis=1)
@@ -59,22 +54,50 @@ def build_and_predict(data):
 
     y_pred = linear.predict(x_test)
 
-    print(x_test)
+    print("y_pred: ", y_pred)
 
     # Accuracy
     accuracy = linear.score(x_test, y_test)
 
-    # Prediction salary with input data from Web Client using LinearRegression
-    input_data = pd.DataFrame([data])
-    predicted_salary = linear.predict(input_data)[0]
-    # Convert decimal to integer
-    if int(predicted_salary) < 0: predicted_salary = 0-int(predicted_salary)
-    predicted_salary = int(predicted_salary)
-    print("{:,.2f}".format(predicted_salary))
+    if type == 'formdata':
 
-    return {
-        "prediction_salary_with_test_data": y_pred.tolist(),
-        "accuracy_test_prediction_salary": accuracy,
-        "predicted_salary": predicted_salary
-    }
+        # Prediction salary with input data from Web Client using LinearRegression
+        input_data = pd.DataFrame([data])
+        print('formdata: ', input_data)
+        predicted_salary = linear.predict(input_data)[0]
+        # Convert decimal to integer
+        if int(predicted_salary) < 0: predicted_salary = 0-int(predicted_salary)
+        predicted_salary = int(predicted_salary)
+        print("{:,.2f}".format(predicted_salary))
+
+        return {
+            "prediction_salary_with_test_data": y_pred.tolist(),
+            "accuracy_test_prediction_salary": accuracy,
+            "predicted_salary": "{:,.2f}".format(predicted_salary)
+        }
+    
+    if type == 'singlefile':
+        try:
+            print('path: ')
+            dataset = pd.read_csv(data)
+            print("olddata: \n", dataset)
+            
+            # remove all column with values not is numbers
+            dataset_filter = dataset.apply(pd.to_numeric, errors='coerce')
+            dataset_filter = dataset_filter.dropna(axis=1)
+
+            input_single = pd.DataFrame(dataset_filter)
+            singlefile_result = linear.predict(input_single)
+            # print("newdata: \n", dataset)
+            dataset['salary'] = singlefile_result
+            # formatted salary column
+            dataset['salary'] = dataset['salary'].map('{:,.2f}'.format)
+
+            print("newdata: \n", dataset)
+
+            return dataset.T.to_dict()
+        except Exception:
+            return {'message': "Error to handle single file"}
+    else:
+        return {'multi files'}
 
